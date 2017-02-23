04e7f1a9aead58522445492564325d8d7aebbd7b
jinzhan <jinzhan@baidu.com>
2017-02-23 09:48:02 +0800

    add css

--- modules/html/course/course.js
+++ modules/html/course/course.js
@@ -1,6 +1,7 @@
 // var $ = require('/modules/lib/jquery.js');
 var Vue = require('/modules/lib/Vue.js');
 var axios = require('/modules/lib/axios.js');
+require('/modules/js/slider.js');
 
 module.exports = {
     init: function () {
@@ -16,12 +17,12 @@ module.exports = {
             methods: {
                 loadData: function () {
                     var $this = this;
-                    axios.get('/ucan/courses/searchCourse', {
+                    axios.get('/view/guest/courses/findList', {
                         params: {}
                     })
                         .then(function (response) {
                             var data = response.data.data;
-                            this.$set(this, 'rows', data.rows);
+                            this.$set(this, 'rows', data);
                         }.bind(this))
                         .catch(function (error) {
                             console.log(error);
